01d7ec38f2e49b40851e71f0c095c86487525b36
Yiqi Sun <ysun26@cmc.edu>
2025-04-17 17:45:43 -0700

    added files

---
+++ load_tweets.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# list all of the files that will be loaded into the database
+# for the first part of this assignment, we will only load a small test zip file with ~10000 tweets
+# but we will write are code so that we can easily load an arbitrary number of files
+
+files='
+test-data.zip
+'
+
+echo 'load normalized'
+for file in $files; do
+	python3 load_tweets.py --db postgresql://postgres:pass@localhost:11064/postgres --inputs "$file" --print_every 10000
+done
+
+for file in $files; do
+     unzip -p "$file" | sed 's/\\u0000//g' | psql postgres://postgres:pass@localhost:10064/postgres -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+    #use SQL's COPY command to load data into pg_denormalized
+done
--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -6,13 +6,26 @@ echo '==========================================================================
 echo 'load pg_denormalized'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
+for file in $files; do
+     unzip -p "$file" | sed 's/\\u0000//g' | psql postgres://postgres:pass@localhost:10064/postgres -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+    #use SQL's COPY command to load data into pg_denormalized
+done
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
+for file in $files; do
+        python3 load_tweets.py --db postgresql://postgres:pass@localhost:11064/postgres --inputs "$file" --print_every 10000
+done
+
+
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
+
+
+
+
