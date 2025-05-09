e393e8faa7edf6921857a844bfe044f7c9213ac3
Yiqi Sun <ysun26@cmc.edu>
2025-05-09 04:46:04 -0700

    modified timing

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -5,15 +5,15 @@ files=$(find data/*)
 echo '================================================================================'
 echo 'load pg_denormalized'
 echo '================================================================================'
-# FIXME: implement this with GNU parallel
+## FIXME: implement this with GNU parallel
 time echo "$files" | parallel ./load_denormalized.sh
-    #use SQL's COPY command to load data into pg_denormalized
-
-
+#    #use SQL's COPY command to load data into pg_denormalized
+#
+#
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-# FIXME: implement this with GNU parallel
+## FIXME: implement this with GNU parallel
 time echo "$files" | parallel ./load_normalized.sh
 
 
